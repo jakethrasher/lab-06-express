3f15a3ac67f41bc9e60868b8f0d0d2c6b3ff10c5
Jake Thrasher <mjakethrasher@gmail.com>
2021-02-18 16:10:57 -0800

    export app

--- index.js
+++ index.js
@@ -1,28 +1,7 @@
-//import express
-const express = require('express');
-const app = express();
-//define port for local development
-const port = 3000;
-const cors = require('cors');
-app.use(cors());
-const { records } = require('./data.js');
+const { app } = require('./app.js');
 
-//app is where we define our routes/endpoints
-app.get('/records', (req, res) => {
+const port = process.env.PORT || 3000;
 
-    res.json({ results: records });
-
-});
-app.get('/records/:id', (req, res) => {
-
-    // const id = Number(req.params.id);
-    // const selectedItem = records.find((item) => item.id === id);
-
-    res.json({ records: records.find(item => item.id === Number(req.params.id)) });
-
-});
-
-//this code will launch/spin up our server
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
